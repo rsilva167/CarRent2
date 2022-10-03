d2d2d602eaec5dee62fdb54120de5476d16f5867
Renan Santos <dosantosrenansilva@gmail.com>
2022-10-03 20:01:25 -0300

    This version was made using interfaces.

---
+++ CarRent2/Program.cs
@@ -0,0 +1,42 @@
+﻿using CarRent2.Entities;
+using CarRent2.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRent2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data: ");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy HH:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/MM/yyyy HH:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalservice = new RentalService(hour, day, new BrazilTaxService());
+
+            rentalservice.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoice: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
